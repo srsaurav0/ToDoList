c57f17684df9bb9c9e8c3cc988c145ac3f1ef780
AbirHasanOnowy <102896787+AbirHasanOnowy@users.noreply.github.com>
2022-11-03 16:43:18 +0600

    Signup Done

---
+++ Login/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by kuet on 3/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func regemail(_ sender: Any) {
+    }
+    @IBAction func regphone(_ sender: Any) {
+    }
+    @IBAction func regpass(_ sender: Any) {
+    }
+    @IBAction func regconpass(_ sender: Any) {
+    }
+    @IBAction func btn(_ sender: Any) {
+    }
+}
+
